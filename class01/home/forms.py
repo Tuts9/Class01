15bbead46d0a4039b066efbb470c08d905f8b3d0
Arthur Rodrigues <rodriguesarthur7777@gmail.com>
2024-06-13 13:13:36 -0300

    feat: Adicionando arquivos ao projeto:
    - assets-p.i/GESTÃO DE PROJETOS.txt
    - assets-p.i/app_artia.jfif
    - assets-p.i/app_asana.jpg
    - assets-p.i/app_trello.jpg
    - assets-p.i/aprendiz.jpg
    - assets-p.i/curso-de-programacao.jpg
    - assets-p.i/django_img.jfif
    - assets-p.i/gestao.png
    - assets-p.i/gestao_p.jfif
    - assets-p.i/pycharm_img.webp
    - assets-p.i/python-jupyter.png
    - assets-p.i/python_18894.png
    - assets-p.i/python_img.jfif
    - class01/db.sqlite3
    
    Modificações realizadas:
    
    - .gitignore
    - class01/templates/base.html
    - class01/home/forms.py
    - class01/home/templates/home.html
    - class01/home/templates/topico.html

--- class01/home/forms.py
+++ class01/home/forms.py
@@ -7,18 +7,18 @@ class SuporteModelForm(forms.ModelForm):
         fields = ['nome', 'email', 'assunto', 'mensagem']
         widgets = {
             'nome': forms.TextInput(attrs={
-                'class': 'block w-full text-gray-700 px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm', 
+                'class': 'block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm', 
                 'placeholder': 'Seu nome'
             }),
             'email': forms.EmailInput(attrs={
-                'class': 'block w-full text-gray-700 px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                'class': 'block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
                 'placeholder': 'Seu email'
             }),
             'assunto': forms.Select(attrs={
-                'class': 'block w-full text-gray-700 px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                'class': 'block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
             }),
             'mensagem': forms.Textarea(attrs={
-                'class': 'block w-full text-gray-700 px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+                'class': 'block w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
                 'placeholder': 'Digite sua mensagem'
             })
         }
