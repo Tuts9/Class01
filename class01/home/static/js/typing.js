21fc4862b073b865241a573ee8b6bc8362e42a25
Arthur Rodrigues <127217432+Tuts9@users.noreply.github.com>
2024-09-20 12:04:36 -0300

    feat: Adição de script para animação da frase inicial do hero-section

---
+++ class01/home/static/js/typing.js
@@ -0,0 +1,6 @@
+var type = new Typed('#frase', {
+    strings: ['Desbloqueie seu potencial de programação!', 'Liberte suas habilidades de programação ao máximo!', 'Alcance o seu máximo na programação!', 'Desperte todo o seu potencial como programador!'],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true
+})
