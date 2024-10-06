f5e636aba9fcb54f7cfa9e0d34ad8ca6c2e098af
Arthur Rodrigues <rodriguesarthur7777@gmail.com>
2024-10-06 18:36:53 -0300

    feat: Adição de script para acessibilidade (aumentar tamanho da fonte) e alterações na velocidade de abertura da sidebar

---
+++ class01/home/static/js/fonts.js
@@ -0,0 +1,27 @@
+const increaseButton = document.getElementById('increase-text');
+const decreaseButton = document.getElementById('decrease-text');
+
+let fontSize = localStorage.getItem('fontSize') ? parseInt(localStorage.getItem('fontSize')) : 100; // 100% é o tamanho padrão
+const minFontSize = 100;  
+const maxFontSize = 150;  // Define o limite em %
+
+function applyFontSize() {
+    document.documentElement.style.fontSize = fontSize + '%'; // Ajusta a fonte no html
+}
+applyFontSize();
+
+increaseButton.addEventListener('click', () => {
+    if (fontSize < maxFontSize) {
+        fontSize += 10;
+        localStorage.setItem('fontSize', fontSize);
+        applyFontSize();
+    }
+});
+
+decreaseButton.addEventListener('click', () => {
+    if (fontSize > minFontSize) {
+        fontSize -= 10;
+        localStorage.setItem('fontSize', fontSize);
+        applyFontSize();
+    }
+});
